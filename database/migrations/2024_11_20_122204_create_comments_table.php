7badd8d868c4397562696e596a8c7557b3e1d1b9
prabhakarsingh1030 <prabhakarsingh1030@gmail.com>
2024-11-24 11:40:56 +0530

    null adede in comments table

--- database/migrations/2024_11_20_122204_create_comments_table.php
+++ database/migrations/2024_11_20_122204_create_comments_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->integer('post_id');
-            $table->integer('parent_comment_id');
+            $table->integer('parent_comment_id')->nullable();
             $table->string('content');
             $table->integer('depth');
             $table->timestamps();
